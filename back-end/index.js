72b7e700bc18d10edab8defd8b52f6134b6c65b7
Joshua Martin <joshua@bluefishds.com>
2018-03-28 15:45:41 -0400

    Add database node stuff

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,9 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const fs = require('fs');
 const zlib = require('zlib');
+const pg = require('pg-promise')();
+const env = require('../config');
+const db = pg(`postgres://${env.user}@localhost:5432/${env.db}`);
 
 var app = new Koa();
 var router = new Router();
