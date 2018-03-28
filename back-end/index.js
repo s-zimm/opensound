5c79e5685f4690e3213b450e28ea663db58daecf
Joshua Martin <joshua@bluefishds.com>
2018-03-28 16:45:03 -0400

    Connect DB with dotenv

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,17 @@
+require('dotenv').config({ path: '../.env'});
 const Koa = require('koa');
 const Router = require('koa-router');
 const fs = require('fs');
 const zlib = require('zlib');
 const pg = require('pg-promise')();
-const env = require('dotenv');
+const db = pg({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER
+});
+
+db.query('SELECT * FROM users').then(data => console.log(data));
 
 var app = new Koa();
 var router = new Router();
