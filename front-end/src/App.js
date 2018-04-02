b3fce53b4d7213074b92b3807040259ec47bec43
Seth <s.zim517@gmail.com>
2018-04-02 10:53:31 -0400

    Adds prompt to name sound and save button with dummy axios post to server

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,10 +5,20 @@ import './App.css';
 import RecordingControls from './components/RecordingControls';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: 1
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <RecordingControls />
+        <RecordingControls 
+          currentUser={this.state.currentUser}
+        />
       </div>
     );
   }
