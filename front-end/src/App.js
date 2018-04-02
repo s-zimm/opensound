905281420683d27c443ba4602254fa5843bc5994
Seth <s.zim517@gmail.com>
2018-04-02 11:31:02 -0400

    Adds rudimentary nav with rudimentary react router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import Nav from './components/Nav';
 import RecordingControls from './components/RecordingControls';
 
 class App extends Component {
@@ -15,11 +17,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <RecordingControls 
-          currentUser={this.state.currentUser}
-        />
-      </div>
+      <Router>
+        <div className="App">
+            <Nav />
+            <Route path="/create-sound" component={RecordingControls}/>
+        </div>
+      </Router>
     );
   }
 }
