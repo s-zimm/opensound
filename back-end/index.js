155b4de64454f8bb702012943a76fa147ffc6aa3
Joshua Martin <joshua@bluefishds.com>
2018-04-03 10:51:21 -0400

    Serve React app from backend

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,23 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const serve = require('koa-static');
 const api = require('./api');
 const fs = require('fs');
 const zlib = require('zlib');
 const parser = require('koa-body')();
 
 let app = new Koa();
+let router = new Router();
+router.use('/api', api.routes());
 
 app.use(parser);
 
-api.use('/api', api.routes());
+app.use(serve('../front-end/build'));
 
-app.use(api.routes());
+app.use(router.routes());
 
 app.use(async ctx => {
-  ctx.body = 'Hello world!';
+  ctx.body = '404 nothing here!';
 });
 
 app.listen(3000);
