24a412522b93ead9656fe6b0fe301bb1e4e14652
Joshua Martin <joshua@bluefishds.com>
2018-04-10 10:49:07 -0400

    Edit init sql file

--- back-end/init.sql
+++ back-end/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(50) UNIQUE NOT NULL,
-  email VARCHAR(50) UNIQUE NOT NULL,
+  username VARCHAR(50) UNIQUE,
+  email VARCHAR(50) UNIQUE,
   first_name VARCHAR(50),
   last_name VARCHAR(50)
 );
@@ -10,7 +10,7 @@ CREATE TABLE sounds (
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users(id),
   title VARCHAR(50),
-  file_path VARCHAR(200) UNIQUE NOT NULL
+  file_path VARCHAR(200) UNIQUE
 );
 
 CREATE TABLE tracks (
@@ -18,7 +18,7 @@ CREATE TABLE tracks (
   user_id INTEGER NOT NULL REFERENCES users(id),
   sound_id INTEGER NOT NULL REFERENCES sounds(id),
   title VARCHAR(50),
-  file_path VARCHAR(200) UNIQUE NOT NULL
+  file_path VARCHAR(200) UNIQUE
 );
 
 INSERT INTO users (username, email, first_name, last_name)
@@ -26,3 +26,6 @@ VALUES ('manuscriptmaster', 'the.manuscriptmaster@gmail.com', 'Joshua', 'Martin'
 
 INSERT INTO users (username, email, first_name, last_name)
 VALUES ('s-zimm', 's.zim517@gmail.com', 'Seth', 'Zimmerman');
+
+INSERT INTO users (username, email, first_name, last_name)
+VALUES ('nybblr', 'the.nybblr@gmail.com', 'Jonathan', 'Martin');
