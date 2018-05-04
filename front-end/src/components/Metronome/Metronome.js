a44e4c4ef198a3659ba25f8e80831d707ca14220
Seth <s.zim517@gmail.com>
2018-05-04 14:10:59 -0400

    Metronome with assets

--- front-end/src/components/Metronome/Metronome.js
+++ front-end/src/components/Metronome/Metronome.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
+import click1 from '../../assets/click1.wav';
+import click2 from '../../assets/click2.wav';
 
 class Metronome extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            bpm: 100
+            bpm: 100,
+            playing: false,
+            count: 0,
+            beatsPerMeasure: 4
         }
+
+        this.click1 = new Audio(click1);
+        this.click2 = new Audio(click2);
+    }
+
+    handleBpmChange = (event) => {
+        const bpm = event.target.value;
+        this.setState({ bpm });
+    }
+
+    playClick = () => {
+        const { count, beatsPerMeasure } = this.state;
+
+        if (count % beatsPerMeasure === 0 ) {
+            this.click2.play();
+        } else {
+            this.click1.play();
+        }
+
+        this.setState(oldstate => ({
+            count: (oldstate.count + 1) % oldstate.beatsPerMeasure
+        }))
+    }
+
+    startStop = () => {
+        if (this.state.playing) {
+            clearInterval(this.timer);
+            this.setState({ playing: false });
+        } else {
+            this.timer = setInterval(this.playClick, (60 / this.state.bpm) * 1000)
+            this.setState({
+                count: 0,
+                playing: true
+            }, this.playClick)
+        }
+        this.click1.play();
     }
 
     render() {
+        const {bpm, playing} = this.state
+
         return (
             <div className="metronome">
                 <div className="bpm-slider">
-                    <div>{this.state.bpm} BPM</div>
+                    <div>{bpm} BPM</div>
                     <input
                         type="range"
                         min="60"
                         max="240"
-                        value={this.state.bpm}
-                        onChange={(event) => this.setState({ bpm: event.target.value })}
+                        value={bpm}
+                        onChange={this.handleBpmChange}
                     />
                 </div>
-                <button>
+                <button onClick={this.startStop}>
                     {this.state.playing ? "Stop" : "Start"}
                 </button>
             </div>
